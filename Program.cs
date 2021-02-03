cd7e9c7d85eb36f8fb2b1f891eb6a64603fcd47e
RocketPrinter <35203190+RocketPrinter@users.noreply.github.com>
2021-02-03 23:24:40 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pic2Chicory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
